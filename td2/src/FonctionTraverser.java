914d9e817b61651b1598fe53c9634b310078cfee
armbrust5u <loup.armbruster7@etu.univ-lorraine.fr>
2025-01-23 12:55:57 +0100

    Exercice 2-1 et 2-2

---
+++ td2/src/FonctionTraverser.java
@@ -0,0 +1,15 @@
+public class FonctionTraverser implements Fonction1D{
+
+    double vEau;
+    double vSol;
+
+    public FonctionTraverser(double eau, double sol){
+        this.vEau = eau;
+        this.vSol = sol;
+    }
+
+    @Override
+    public double getF(double x) {
+        return (1/vEau)*Math.sqrt(Math.pow(500-x, 2)+50*50)+(1/vSol)*Math.sqrt(x*x+450*450);
+    }
+}
--- td2/src/Main.java
+++ td2/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
+        FonctionTraverser ft = new FonctionTraverser(3000, 15000);
+
         System.out.println("Hello world!");
     }
 }
