e6fabeeb4f8a95b3388c9518d4dfffe42d3b9a64
armbrust5u <loup.armbruster7@etu.univ-lorraine.fr>
2025-01-23 12:58:49 +0100

    2-3

--- td2/src/Main.java
+++ td2/src/Main.java
@@ -3,6 +3,10 @@ public class Main {
 
         FonctionTraverser ft = new FonctionTraverser(3000, 15000);
 
-        System.out.println("Hello world!");
+        System.out.println("lorsque le point A est a la verticale du point B, la duree de deplacement est d’environ 0.061512 h");
+        System.out.println(ft.getF(500));
+
+        System.out.println("lorsque le point A est `a la verticale du point A, la dur´ee de d´eplacement est d’environ 0.197497 h.");
+        System.out.println(ft.getF(0));
     }
 }
