8b71f6d634b9963a8502b5bf7a14070afae864bb
armbrust5u <loup.armbruster7@etu.univ-lorraine.fr>
2025-01-31 16:46:37 +0100

    fin exercice 2

--- td2/src/MainEncadrerDerivee.java
+++ td2/src/MainEncadrerDerivee.java
@@ -10,5 +10,11 @@ public class MainEncadrerDerivee {
         EncadrerDerivee ed = new EncadrerDerivee(fct);
 
         System.out.printf("\nPour la fonction (x − 2) ∗ (x − 1) l'optimum local est environ %2.7f", ed.encadrer(0, 4));
+
+        FonctionTraverser ft = new FonctionTraverser(3000, 15000);
+
+        EncadrerDerivee ed2 = new EncadrerDerivee(ft);
+
+        System.out.printf("\nPour la fonction (x − 2) ∗ (x − 1) l'optimum local est environ %2.7f", ed2.encadrer(0, 500));
     }
 }
