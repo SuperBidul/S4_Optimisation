d07582091888d61d0175c74b89e0020558fb7304
armbrust5u <loup.armbruster7@etu.univ-lorraine.fr>
2025-02-06 10:25:46 +0100

    ajout Main meilleur cercle fin td2

--- td2/src/main/MainCercle.java
+++ td2/src/main/MainCercle.java
@@ -1,4 +1,31 @@
 package main;
 
+import cercle.ProblemeCercle;
+import encadrement.EncadrerDerivee;
+import fonction.cercle.FonctionRayon;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainCercle {
+    public static void main(String[] args) {
+        // Liste de points
+        List<Point2D> points = new ArrayList<>();
+        points.add(new Point2D.Double(0, 100));
+        points.add(new Point2D.Double(70, 50));
+        points.add(new Point2D.Double(-70, 70));
+        points.add(new Point2D.Double(70, -80));
+        points.add(new Point2D.Double(-90, -80));
+        points.add(new Point2D.Double(-10, -80));
+
+        // Initialisation du problème
+        ProblemeCercle pc = new ProblemeCercle(points);
+
+        // Recherche du meilleur rayon pour un cercle centré en (0,0)
+        FonctionRayon fr = new FonctionRayon(pc, 0, 0);
+
+        EncadrerDerivee ed = new EncadrerDerivee(fr);
+        System.out.printf("\nLe meilleur rayon trouvé est : %2.7f", ed.encadrer(0, 200));
+    }
 }
