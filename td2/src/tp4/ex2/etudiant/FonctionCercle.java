a82a3cb7e96ad5b4f434b2ed0bd79a276e213f42
armbrust5u <loup.armbruster7@etu.univ-lorraine.fr>
2025-02-13 23:22:00 +0100

    ajout 2-4

---
+++ td2/src/tp4/ex2/etudiant/FonctionCercle.java
@@ -0,0 +1,18 @@
+package tp4.ex2.etudiant;
+
+import tp4.ex1.fonctionD.FonctionD;
+
+public class FonctionCercle extends FonctionD {
+
+    protected ProblemeRF probleme;
+
+    public FonctionCercle(ProblemeRF probleme) {
+        this.probleme = probleme;
+    }
+
+    @Override
+    public double getValeur(double[] x) {
+        Cercle c = new Cercle(x[0], x[1], x[2]);
+        return probleme.evaluer(c);
+    }
+}
